d539ea748f6e46dde2ff06e2bd06f1b512bb0899
Justin Miller <indymiller@gmail.com>
2020-09-21 19:04:11 -0400

    added search form to page to allow for searching different github users

--- src/App.js
+++ src/App.js
@@ -1,66 +1,71 @@
 import React, { useState, useEffect } from 'react';
+// https://www.youtube.com/watch?v=bU_eq8qyjic
+import { useForm } from 'react-hook-form';
 import './App.css';
 import { Orgs } from './components/Orgs';
 import { Repos } from './components/Repos';
 import { User } from './components/User';
 import { getRepos, getUserData } from './github-api';
 
-
-// https://jsonplaceholder.typicode.com/users
-// https://jsonplaceholder.typicode.com/albums
-
-const userId = "millerjl1980"
+const defaultUser = "millerjl1980"
 
 const App = () => {
 
+  const [searchId, setSearchId] = useState([defaultUser]);
+  const {register, handleSubmit} = useForm();
   const [user, setUser] = useState([]);
   const [orgs, setOrgs] = useState([]);
   const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState([true]);
   
+  const onSubmit = (data) => {
+    setSearchId(data['searchId'])
+    console.log(searchId)
+  }
+
   useEffect(() => {
       const fetchUser = async () => {
-        const userData = await getUserData([userId])
+        const userData = await getUserData([searchId])
         setUser(userData['user'])
-        setLoading(false);
       }
 
       const fetchOrgs = async () => {
-        const userData = await getUserData([userId])
+        const userData = await getUserData([searchId])
         setOrgs(userData['orgs'])
-        setLoading(false);
       }
 
       const fetchRepos = async () => {
-        setRepos( await getRepos([userId]));
-        setLoading(false);
+        setRepos( await getRepos([searchId]));
       }
 
       fetchUser();
       fetchRepos();
       fetchOrgs();
 
-  }, []);
-
-  // console.log(user)
-  //console.log(repos)
-  // console.log(orgs)
-
-  if (loading) {
-    return <div className="container"><h2>Loading...</h2></div>;
-  }
+  }, [searchId]);
 
   return (
     <div className="container">
-      <h1>My App</h1>
+      <h1>GitHub User</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} >
+      <div className="form-group">
+        <label for="exampleInputEmail1">GitHub User ID</label>
+        <input type="text" name="searchId" className="form-control" id="exampleInputEmail1" aria-describedby="textHelp" placeholder="Enter user ID" ref={register}/>
+        <small id="textHelp" className="form-text text-muted">Enter a GitHub user id to search for.</small>
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+
       <User 
         image= {user['avatar_url']}
         name= {user['name']}
         bio= {user['bio']}
         followers= {user['followers']}
       />
+
       <h2>Organizations</h2>
       <Orgs orgs={ orgs } />
+
       <h2>Repositories</h2>
       <Repos repos={ repos } />
 
