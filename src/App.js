3bc80cf22d627ab431905b0c78b630b7b8d4bc2d
Justin Miller <indymiller@gmail.com>
2020-09-21 17:18:22 -0400

    added api file and started components for structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { getRepos, getUserData } from './github-api';
 
-function App() {
+
+// https://jsonplaceholder.typicode.com/users
+// https://jsonplaceholder.typicode.com/albums
+
+const userId = "millerjl1980"
+
+const App = () => {
+  
+  const [users, setUsers] = useState([]);
+  const [orgs, setOrgs] = useState([]);
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState([true]);
+  
+  useEffect(() => {
+      const fetchUsers = async () => {
+        const userData = await getUserData([userId])
+        setUsers(userData['user'])
+        setLoading(false);
+      }
+
+      const fetchOrgs = async () => {
+        const userData = await getUserData([userId])
+        setOrgs(userData['orgs'])
+        setLoading(false);
+      }
+
+      const fetchRepos = async () => {
+        setRepos( await getRepos([userId]));
+        setLoading(false);
+      }
+
+      fetchUsers();
+      fetchRepos();
+      fetchOrgs();
+
+  }, []);
+
+  console.log(users)
+  console.log(repos)
+  console.log(orgs)
+
+  if (loading) {
+    return <div className="container"><h2>Loading...</h2></div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>My App</h1>
     </div>
   );
 }
---
+++ src/components/User.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const User = ({ user }) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
